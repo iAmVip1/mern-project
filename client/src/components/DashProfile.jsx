7c16251865ea67a929f6ef450982d8e84d584bf7
iAmVip1 <gp16373@gmail.com>
2024-07-25 17:37:08 +0545

    sign out mistaken

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -26,7 +26,7 @@ export default function DashProfile() {
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span className='cursor-pointer'>Delete Account</span>
-        <span className='cursor-pointer'>Delete Account</span>
+        <span className='cursor-pointer'>Sign Out</span>
       </div>
     </div>
   )
