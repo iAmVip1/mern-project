0322b5a4dc267e4d379ab2f6fa2900e85e7981b2
iAmVip1 <gp16373@gmail.com>
2024-08-04 08:45:57 +0545

    create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.routes.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -29,6 +30,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/listing', listingRouter);
 
 app.use ((err, req, res, next) => {
   const statusCode = err.statusCode || 500
---
+++ api/models/listing.model.js
@@ -0,0 +1,75 @@
+import mongoose from "mongoose";
+
+const listingSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        regularPrice:{
+            type: Number,
+            required: true,
+        },
+        phoneNumber: {
+            type: Number,
+            required: true,
+        },
+        plumber: {
+            type: Boolean,
+            required: true,
+        },
+        medical: {
+            type: Boolean,
+            required: true,
+        },
+        mechanics: {
+            type: Boolean,
+            required: true,
+        },
+        electrician: {
+            type: Boolean,
+            required: true,
+        },
+        Driver: {
+            type: Boolean,
+            required: true,
+        },
+        electrician: {
+            type: Boolean,
+            required: true,
+        },
+        civilEngineer: {
+            type: Boolean,
+            required: true,
+        },
+        catering: {
+            type: Boolean,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+        },
+        imageUrls: {
+            type: Array,
+            required: true,
+        },
+        userRef: {
+            type: String,
+            required: true,
+        },
+    }, 
+    {timestamps: true}
+)
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+export default Listing;
