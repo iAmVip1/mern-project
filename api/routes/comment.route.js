3c666ff07d3ceccc333a453c2682156c50d0316a
iAmVip1 <gp16373@gmail.com>
2024-09-29 17:39:43 +0545

    add likes in the comment

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import {
-  createComment, getListingComments
+  createComment, getListingComments, likeComment
   
 } from '../controllers/comment.controller.js';
 
@@ -9,6 +9,7 @@ const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
 router.get('/getListingComments/:listingId', getListingComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
 
 
 export default router;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,7 +1,7 @@
 import { Alert, Button, Modal, TextInput, Textarea } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Comment from './Comment';
 
 export default function CommentSection({listingId}) {
@@ -9,6 +9,7 @@ export default function CommentSection({listingId}) {
     const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState(null);
     const [comments, setComments] = useState([]);
+    const navigate = useNavigate();
     console.log(comments);
     
     const handleSubmit = async (e) => {
@@ -55,6 +56,37 @@ export default function CommentSection({listingId}) {
       }
       getComments();
     }, [listingId])
+
+    const handleLike = async (commentId) => {
+      try {
+        if (!currentUser) {
+          navigate('/sign-in');
+          return;
+        }
+        const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+          method: 'PUT',
+        });
+        if (res.ok) {
+          const data = await res.json();
+          setComments(
+            comments.map((comment) =>
+              comment._id === commentId
+                ? {
+                    ...comment,
+                    likes: data.likes,
+                    numberOfLikes: data.likes.length,
+                  }
+                : comment
+            )
+          );
+        }
+        
+      } catch (error) {
+        console.log(error.message);
+        
+      }
+      
+    }
 
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
@@ -111,7 +143,7 @@ export default function CommentSection({listingId}) {
           {comments.map((comment) => (
             <Comment
             key={comment._id}
-              comment={comment}
+              comment={comment} onLike={handleLike}
             />
           ))}
           </>
