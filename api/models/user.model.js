a72cf8776c7789faecef88356defc2bca9871ca6
iAmVip1 <gp16373@gmail.com>
2024-11-10 16:50:17 +0545

    is worker added

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -23,6 +23,10 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    isWorker: {
+        type: Boolean,
+        default: false,
+    },
 }, 
 {timestamps:true}
 );
