46103fe751fbf44307fdc5bb5bda9aff1a1ed87d
iAmVip1 <gp16373@gmail.com>
2024-11-12 17:45:24 +0545

    delete application

--- api/controllers/application.controller.js
+++ api/controllers/application.controller.js
@@ -10,3 +10,20 @@ export const createApplication = async (req, res, next) =>{
         
     }
 }
+
+export const deleteApplication = async (req, res, next) =>{
+    const application = await Application.findById(req.params.id);
+
+    if (!application) {
+        return next (errorHandler(404, 'Application not found'));
+    }
+    if ( !req.user.isAdmin && req.user.id !== application.userRef.toString()) {
+        return next(errorHandler(401, 'You can only delete your own applications!'));
+    }
+    try {
+        await Application.findByIdAndDelete(req.params.id);
+        res.status(200).json('Application has been deleted! ')
+    } catch (error) {
+        next(error);
+    }
+}
--- api/routes/application.route.js
+++ api/routes/application.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { createApplication } from '../controllers/application.controller.js';
+import { createApplication, deleteApplication } from '../controllers/application.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createApplication);
-
+router.delete('/delete/:id', verifyToken, deleteApplication);
 export default router;
