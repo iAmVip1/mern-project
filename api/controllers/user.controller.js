d06a085ed9310ef033e70094d76f8df7cd76e674
iAmVip1 <gp16373@gmail.com>
2024-11-14 10:54:41 +0545

    is worker api done

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -47,6 +47,7 @@ export const updateUser = async (req, res, next) => {
               email: req.body.email,
               profilePicture: req.body.profilePicture,
               password: req.body.password,
+              isWorker: req.body.isWorker,
             },
           },
           { new: true }
