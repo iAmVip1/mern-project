91f75cc723f3f8085ace53d92e73862d884583e2
iAmVip1 <gp16373@gmail.com>
2024-11-16 21:14:39 +0545

    recommendation done

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import {Swiper, SwiperSlide} from 'swiper/react'
 import SwiperCore from 'swiper';
 import { useSelector } from 'react-redux';
@@ -19,6 +19,7 @@ import {
   import { Button } from "flowbite-react";
 import Contact from '../components/Contact';
 import CommentSection from '../components/CommentSection';
+import ListingItem from '../components/ListingItem';
 
 export default function Listing() {
     SwiperCore.use([Navigation]);
@@ -28,6 +29,19 @@ export default function Listing() {
     const [ error, setError] = useState(false);
     const [contact, setContact] = useState(false);
     const params = useParams();
+
+    // recommend in posts
+    const [dayListings, setDayListings] = useState([]);
+    const [hourListings, setHourListings] = useState([]);
+    const [medicalListings, setMedicalListings] = useState([]);
+    const [electricianListings, setElectricianListings] = useState([]);
+    const [driverListings, setDriverListings] = useState([]);
+    const [cateringListings, setCateringListings] = useState([]);
+    const [plumberListings, setPlumberListings] = useState([]);
+    const [civilEngineerListings, setCivilEngineerListings] = useState([]);
+
+    // recommend in ends here
+
     const { currentUser } = useSelector((state) => state.user);
     useEffect(() =>{
         const fetchListing = async () => {
@@ -44,11 +58,93 @@ export default function Listing() {
                 setListing(data);
                 setLoading(false);
                 setError(false);
+                fetchDayListings()
             } catch (error) {
                 setError(true)
                 setLoading(false);
             }
         }
+        const fetchDayListings = async () => {
+          try {
+            const res = await fetch('/api/listing/get?mechanics=true&limit=3');
+            const data = await res.json();
+            setDayListings(data);
+            fetchHourListings();
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        const fetchHourListings = async () => {
+          try {
+            const res = await fetch('/api/listing/get?uncategorized=true&limit=3');
+            const data = await res.json();
+            setHourListings(data);
+            fetchPlumberListings();
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        
+        const fetchPlumberListings = async () => {
+          try {
+            const res = await fetch('/api/listing/get?plumber=true&limit=3');
+            const data = await res.json();
+            setPlumberListings(data);
+            fetchMedicalListings();
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        const fetchMedicalListings = async () => {
+          try {
+            const res = await fetch('/api/listing/get?medical=true&limit=3');
+            const data = await res.json();
+            setMedicalListings(data);
+            fetchCateringListings();
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        const fetchCivilEngineerListings = async () => {
+          try {
+            const res = await fetch('/api/listing/get?civilEngineer=true&limit=3');
+            const data = await res.json();
+            setCivilEngineerListings(data);
+            
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        const fetchElectricianListings = async () => {
+          try {
+            const res = await fetch('/api/listing/get?electrician=true&limit=3');
+            const data = await res.json();
+            setElectricianListings(data);
+            fetchCivilEngineerListings();
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        const fetchDriverListings = async () => {
+          try {
+            const res = await fetch('/api/listing/get?driver=true&limit=3');
+            const data = await res.json();
+            setDriverListings(data);
+            fetchElectricianListings()
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        const fetchCateringListings = async () => {
+          try {
+            const res = await fetch('/api/listing/get?catering=true&limit=3');
+            const data = await res.json();
+            setCateringListings(data);
+            fetchDriverListings();
+          } catch (error) {
+            console.log(error);
+          }
+        };
         fetchListing();
     }, [params.listingId]);
     
@@ -123,50 +219,6 @@ export default function Listing() {
               </li>
              </ul>
               )}
-             {/* <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6 mt-3 dark:text-white'>
-
-             <li className='flex items-center gap-1 whitespace-nowrap '>
-                
-                {listing.plumber ? 'Plumber' : ''}
-              </li>
-              
-              <li className='flex items-center gap-1 whitespace-nowrap '>
-                
-                {listing.medical ? 'Medical' : ''}
-              </li>
-
-              <li className='flex items-center gap-1 whitespace-nowrap '>
-                
-                {listing.mechanics ? 'Mechanics' : ''}
-              </li>
-
-              <li className='flex items-center gap-1 whitespace-nowrap '>
-                
-                {listing.electrician ? 'Electrician' : ''}
-              </li>
-
-              <li className='flex items-center gap-1 whitespace-nowrap '>
-                
-                {listing.driver ? 'Driver' : ''}
-              </li>
-
-              <li className='flex items-center gap-1 whitespace-nowrap '>
-                
-                {listing.civilEngineer ? 'Civil Engineer' : ''}
-              </li>
-
-              <li className='flex items-center gap-1 whitespace-nowrap '>
-                
-                {listing.catering ? 'Catering' : ''}
-              </li>
-
-              <li className='flex items-center gap-1 whitespace-nowrap '>
-                
-                {listing.uncategorized ? 'Uncategorized' : ''}
-              </li>
-
-             </ul>
-              */}
 
     <div className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6 mt-3 dark:text-white'>
                 
@@ -247,7 +299,220 @@ export default function Listing() {
             {contact && <Contact listing={listing} />}
           </div>
           <CommentSection listingId={listing._id} />
+
+            {/* recommendation ui */}
+
+          <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+            
+            {/* mechanics */}
+            <div className="">
+              
+          {
+                    listing.mechanics && (
+                        <div className="">
+                          {dayListings && dayListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>More related posts for Mechanics </h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?mechanics=true'}>Show more </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {dayListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+              
+            </div>
+          </div>
+        )}
+                        </div>
+                    )
+                }
+            </div>
+
+            {/* plumber */}
+            <div className="">
+              
+          {
+                    listing.plumber && (
+                        <div className="">
+                          {plumberListings && plumberListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>More related posts for Plumber </h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?plumber=true'}>Show more </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {plumberListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+              
+            </div>
+          </div>
+        )}
+                        </div>
+                    )
+                }
+            </div>
+          
+
+            {/* civilEngineer */}
+            <div className="">
+              
+          {
+                    listing.civilEngineer && (
+                        <div className="">
+                          {civilEngineerListings && civilEngineerListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>More related posts for Civil Engineer </h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?civilEngineer=true'}>Show more </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {civilEngineerListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+              
+            </div>
+          </div>
+        )}
+                        </div>
+                    )
+                }
+            </div>
+          
+
+            {/* medical */}
+            <div className="">
+              
+          {
+                    listing.medical && (
+                        <div className="">
+                          {medicalListings && medicalListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>More related posts for Medical </h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?medical=true'}>Show more </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {medicalListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+              
+            </div>
+          </div>
+        )}
+                        </div>
+                    )
+                }
+            </div>
+          
+
+            {/* electrician */}
+            <div className="">
+              
+          {
+                    listing.electrician && (
+                        <div className="">
+                          {electricianListings && electricianListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>More related posts for Electrician </h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?electrician=true'}>Show more </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {electricianListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+              
+            </div>
+          </div>
+        )}
+                        </div>
+                    )
+                }
+            </div>
+          
+
+            {/* driver */}
+            <div className="">
+              
+          {
+                    listing.driver && (
+                        <div className="">
+                          {driverListings && driverListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>More related posts for Driver </h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?driver=true'}>Show more </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {driverListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+              
+            </div>
+          </div>
+        )}
+                        </div>
+                    )
+                }
+            </div>
+          
+
+            {/* catering */}
+            <div className="">
+              
+          {
+                    listing.catering && (
+                        <div className="">
+                          {cateringListings && cateringListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>More related posts for Catering </h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?catering=true'}>Show more </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {cateringListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+              
+            </div>
+          </div>
+        )}
+                        </div>
+                    )
+                }
+            </div>
+          
+
+            {/* uncategorized */}
+            <div className="">
+              
+          {
+                    listing.uncategorized && (
+                        <div className="">
+                          {hourListings && hourListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>More related posts for Uncategorized </h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?uncategorized=true'}>Show more </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {hourListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+              
+            </div>
+          </div>
+        )}
+                        </div>
+                    )
+                }
+            </div>
+          
+          </div>
         </div>
+        
         }
     </main>
   )
